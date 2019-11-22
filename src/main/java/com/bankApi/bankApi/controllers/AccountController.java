73cdae75a6c12ce6a4f6e37d660258bfe09555f6
Sarah <sarahhargreaves10@gmail.com>
2019-11-22 18:29:53 -0500

    finished account service and controller

--- src/main/java/com/bankApi/bankApi/controllers/AccountController.java
+++ src/main/java/com/bankApi/bankApi/controllers/AccountController.java
@@ -8,38 +8,40 @@ import com.bankApi.bankApi.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class AccountController {
-
-    @Autowired
     private AccountService accountService;
 
-    @Autowired
     private CustomerService customerService;
+
+    @Autowired
+    public AccountController(AccountService accountService, CustomerService customerService) {
+        Assert.notNull(accountService, "Account Service must not be null.");
+        Assert.notNull(customerService, "Customer Service must not be null.");
+        this.accountService = accountService;
+        this.customerService = customerService;
+    }
 
     @GetMapping("/accounts")
     public ResponseEntity<?> getAllAccounts(){
         HttpStatus statusCode;
         Response response = new Response();
         List<Account> r = accountService.findAll();
-//        To check if a list is empty add .isEmpty to your method.
         if(r.isEmpty()){
             response.setCode(404);
-            response.setMessage("error fetching accounts");
-//            StatusCode for 404.
+            response.setMessage("No accounts found.");
             statusCode = HttpStatus.NOT_FOUND;
         }else {
             response.setCode(200);
             response.setMessage("Success");
             response.setData(r);
-//            StatusCode for 200.
             statusCode = HttpStatus.OK;
         }
         return new ResponseEntity<>(response, statusCode);
@@ -51,13 +53,12 @@ public class AccountController {
         Response response = new Response();
         if (!accountService.existsById(id)) {
             response.setCode(404);
-            response.setMessage("error fetching account");
+            response.setMessage("No account with ID " + id + " Found.");
             statusCode = HttpStatus.NOT_FOUND;
         } else {
-            Optional<Account> a = accountService.findById(id);
+            Account a = accountService.findById(id);
             response.setCode(200);
-            response.setMessage("error fetching account");
-//            The singletonList() method of java.util.Collections class is used to return an immutable list containing only the specified object.
+            response.setMessage("Success");
             response.setData(Collections.singletonList(a));
             statusCode = HttpStatus.OK;
 
@@ -71,10 +72,9 @@ public class AccountController {
         Response response = new Response();
         if(!customerService.existsById(id)){
             response.setCode(404);
-            response.setMessage("error fetching customers account");
+            response.setMessage("No customer with ID " + id + " Found.");
             statusCode = HttpStatus.NOT_FOUND;
         } else {
-//            Trying to find customers by ID.
             List<Account> y = accountService.findAllByCustomerId(id);
             response.setData(y);
             response.setCode(200);
@@ -90,17 +90,17 @@ public class AccountController {
         HttpStatus statusCode;
         if(!customerService.existsById(id)){
             response.setCode(404);
-            response.setMessage("error creating customers account");
+            response.setMessage("No customer with ID " + id + " Found.");
             statusCode = HttpStatus.NOT_FOUND;
         }else{
             Customer customer = customerService.findById(id);
-            account.setCustomerId(id);
-            accountService.save(account);
+            account.setCustomerId(customer.getCustomerId());
+            Account savedAccount = accountService.save(account);
             response.setCode(201);
             ArrayList<Account> accounts = new ArrayList<>();
-            accounts.add(account);
+            accounts.add(savedAccount);
             response.setData(accounts);
-            response.setMessage("Account created");
+            response.setMessage("Account created.");
             statusCode = HttpStatus.OK;
         }
         return new ResponseEntity<>(response, statusCode);
@@ -109,17 +109,16 @@ public class AccountController {
     public ResponseEntity<?> updateAccounts(@RequestBody Account account, @PathVariable("id") long id){
         Response response = new Response();
         HttpStatus statusCode;
-        Optional<Account> g = accountService.findById(id);
         if(!accountService.existsById(id)){
             response.setCode(404);
-            response.setMessage("Error");
+            response.setMessage("No account with ID " + id + " Found.");
             statusCode = HttpStatus.NOT_FOUND;
         } else {
-            accountService.updateAccount(account, id);
-            response.setCode(200);
-            response.setMessage("Customer account updated");
+            Account g = accountService.updateAccount(account, id);
+            response.setCode(202);
+            response.setMessage("Account updated");
             response.setData(Collections.singletonList(g));
-            statusCode = HttpStatus.OK;
+            statusCode = HttpStatus.ACCEPTED;
         }
         return new ResponseEntity<>(response, statusCode);
     }
@@ -129,7 +128,7 @@ public class AccountController {
         HttpStatus statusCode;
         if(!accountService.existsById(id)) {
             response.setCode(404);
-            response.setMessage("Account does not exists");
+            response.setMessage("No account with ID " + id + " Found.");
             statusCode = HttpStatus.NOT_FOUND;
         }else{
             accountService.deleteById(id);
